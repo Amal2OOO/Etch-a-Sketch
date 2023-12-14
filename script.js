718f0d0a546e3f97c1e0ca418e45eb2d4c93e756
Amal2OOO <147815476+Amal2OOO@users.noreply.github.com>
2023-12-14 14:28:05 +0500

    add size button

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 
 const grid = document.querySelector('.grid');
+const gridSize = document.querySelector('.gridSize');
+let gridElements = document.querySelectorAll('.gridElement');
 
 
 let gridQuantity = 10;
-let gridSquare = gridQuantity * gridQuantity;
 
-for(let i = 0; i < gridSquare; i++) {
-  const gridElement = document.createElement('div');
-  gridElement.className = 'gridElement';
-  gridElement.style.height = grid.clientHeight / gridQuantity + 'px';
-  gridElement.style.width = grid.clientWidth / gridQuantity + 'px';
-  grid.appendChild(gridElement);
+createGrid(gridQuantity);
 
-  gridElement.addEventListener('mouseover', () => {
-    gridElement.style.backgroundColor = 'black';
-  });
+function createGrid() {
+  let gridSquare = gridQuantity * gridQuantity;
+
+  for(let i = 0; i < gridSquare; i++) {
+    const gridElement = document.createElement('div');
+    gridElement.className = 'gridElement';
+    gridElement.style.height = grid.clientHeight / gridQuantity + 'px';
+    gridElement.style.width = grid.clientWidth / gridQuantity + 'px';
+    grid.appendChild(gridElement);
+
+    gridElement.addEventListener('mouseover', () => {
+      gridElement.style.backgroundColor = 'black';
+    });
+  }
 }
 
+gridSize.addEventListener('click', () => {
+  gridQuantity = Number(prompt('Enter grid size:'));
+  while(grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+  createGrid(gridQuantity);
+});
